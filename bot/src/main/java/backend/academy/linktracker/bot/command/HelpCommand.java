2d53d202d407c09c1f01f548a35240883e2aeb25
Шестопалов Пётр <shestikpetr@gmail.com>
2026-02-17 04:45:21 +0700

    refactor(bot): используем ApplicationContext, чтобы отображать абсолютно все команды

--- bot/src/main/java/backend/academy/linktracker/bot/command/HelpCommand.java
+++ bot/src/main/java/backend/academy/linktracker/bot/command/HelpCommand.java
@@ -2,16 +2,16 @@ package backend.academy.linktracker.bot.command;
 
 import com.pengrad.telegrambot.model.Update;
 import com.pengrad.telegrambot.request.SendMessage;
+import org.springframework.context.ApplicationContext;
 import org.springframework.stereotype.Component;
-import java.util.List;
 import java.util.stream.Collectors;
 
 @Component
 public class HelpCommand implements Command {
-    private final List<Command> commands;
+    private final ApplicationContext context;
 
-    public HelpCommand(List<Command> commands) {
-        this.commands = commands;
+    public HelpCommand(ApplicationContext context) {
+        this.context = context;
     }
 
     @Override
@@ -26,7 +26,7 @@ public class HelpCommand implements Command {
 
     @Override
     public SendMessage handle(Update update) {
-        String text = commands.stream()
+        String text = context.getBeansOfType(Command.class).values().stream()
                 .map(command -> command.command() + " - " + command.description())
                 .collect(Collectors.joining("\n"));
 
