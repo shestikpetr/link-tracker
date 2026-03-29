542f75755ccef9c0f22fbb57411013464e6b3187
Шестопалов Пётр <shestikpetr@gmail.com>
2026-03-29 23:47:41 +0700

    refactor(bot): мелкие исправления в StateInputDispatcher

--- bot/src/main/java/backend/academy/linktracker/bot/listener/StateInputDispatcher.java
+++ bot/src/main/java/backend/academy/linktracker/bot/listener/StateInputDispatcher.java
@@ -8,6 +8,7 @@ import com.pengrad.telegrambot.request.SendMessage;
 import java.util.EnumMap;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 
@@ -19,29 +20,36 @@ public class StateInputDispatcher {
 
     public StateInputDispatcher(List<StateHandler> stateHandlers, BotClient botClient) {
         this.botClient = botClient;
-        this.stateHandlers = new EnumMap<>(ChatState.class);
-        for (StateHandler cmd : stateHandlers) {
-            this.stateHandlers.put(cmd.handledState(), cmd);
-        }
+        this.stateHandlers = stateHandlers.stream()
+                .collect(Collectors.toMap(
+                        StateHandler::handledState,
+                        handler -> handler,
+                        (_, b) -> b,
+                        () -> new EnumMap<>(ChatState.class)));
     }
 
     public void dispatch(ChatState state, Update update) {
         StateHandler cmd = stateHandlers.get(state);
-        if (cmd != null) {
-            long chatId = update.message().chat().id();
-            SendMessage response;
-            try {
-                response = cmd.handleInput(update);
-            } catch (Exception e) {
-                log.atError()
-                        .setMessage("Ошибка при обработке состояния")
-                        .addKeyValue("state", state)
-                        .addKeyValue("chatId", chatId)
-                        .setCause(e)
-                        .log();
-                response = new SendMessage(chatId, "Произошла внутренняя ошибка.");
-            }
-            botClient.execute(response);
+
+        if (cmd == null) {
+            return;
         }
+
+        long chatId = update.message().chat().id();
+        SendMessage response;
+
+        try {
+            response = cmd.handleInput(update);
+        } catch (Exception e) {
+            log.atError()
+                    .setMessage("Ошибка при обработке состояния")
+                    .addKeyValue("state", state)
+                    .addKeyValue("chatId", chatId)
+                    .setCause(e)
+                    .log();
+            response = new SendMessage(chatId, "Произошла внутренняя ошибка.");
+        }
+
+        botClient.execute(response);
     }
 }
